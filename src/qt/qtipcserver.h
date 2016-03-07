306ccbc25dbea131c8b79ee53ac9653b1de333cf
RebelDev <RebelDev>
2016-03-08 01:01:13 +0500

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Rebel-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "RebelURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
